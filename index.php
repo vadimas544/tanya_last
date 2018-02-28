9a42d1fe2e927da4dba4d17671adc4fb3ba01e00
Vadim Samoilenko <vadim123544@gmail.com>
2018-02-28 23:06:54 +0200

    Tanya site v1.0

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> 
+  </head>
+
+  <body>
+	<div class="container-fluid">
+		<div class="row">
+      <div class="col-md-12">
+          <div class="logo">
+          </div>
+      </div>
+    </div><!-- end logo-->
+      <div class="row">
+       <div class="col-md-12">
+          <nav>
+            <div class="col-md-4 col-md-offset-4 nav-menu">
+              <ul>
+                <li><a href="#"><span class="first-menu">Home</span></a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Book online</a></li>
+                <li><a href="#">Blog</a></li>
+              </ul>
+            </div>
+            <i class="fab fa-instagram"></i>
+          </nav>
+        </div>
+      </div>
+     <!-- <div class="row">
+        <div class="col-md-4 col-md-offset-4 boots-menu">
+          <ul class="nav justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+             <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>-->
+  </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
