77cad0ee03ce71ac2241b63e8484df7e3b9933b7
Vadim Samoilenko <vadim_rt51@ukr.net>
2018-03-22 19:39:14 +0200

    Site with slider

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fontawesome.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> 
   </head>
@@ -12,48 +13,190 @@
   <body>
 	<div class="container-fluid">
 		<div class="row">
-      <div class="col-md-12">
-          <div class="logo">
+            <div class="col-md-12">
+                <div class="logo">
+                </div>
+            </div>
+    </div><!-- end logo-->
+        <div id="mainmenu">
+            <ul>
+            <li class="active1"><a class="active1" href="#"> Home </a></li>
+            <li><a href="#"> Shop </a></li>
+            <li><a href="#"> Book online </a></li>
+            <li><a href="#"> Blog </a></li>
+            </ul>
+        </div>
+            <header>
+      <div class="row">
+            <div class="col-md-12">
+          <div class="header-logo text-center">
+            <h1>Welcome to<br /> Bright<br /> paints</h1>
+            <h3>Exactly What You Need</h3>
+            <a class="transition" href="#">Contact</a>
+          </div>
+            </div>
+          </div>
+    </header>
+    <section>
+      <div class="row">
+        <div class="col-md-12">
+            <img class="img-responsive center-block img-rounded" src="img/2.jpg" alt="image"/>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="row">
+          <h3>Our Story</h3>  
+        </div>
+        <div class="row">
+          <h1>About</h1>
+        </div>
+        <div class="row">
+          <h5>Since being established in 2000, Bright paints has been known for an unparalleled commitment to customer <br />satisfaction. It’s this standard of excellence that has provided the impetus for us to grow into the business we <br />are today. For more information about the products and services we provide, reach out today.</h5>
+        </div>
+      </div>
+    </section>
+    <div class="section1">
+          <div class="sec1">
+            <div class="wrapper">
+              <div class="row">
+                <div class="col-md-12 wrapper">
+                  <p>Bright paints Services</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="sec1_1">
+            <div class="row">
+              <div class="col-md-6 text-center text_sec">
+                <h3 class="section_h3">Customer Support</h3>
+                <h4 class="section_h4">Next Level Service</h4>
+                <a class="transition section_a" href="#">Get in touch</a>
+              </div>
+              <div class="col-md-6">
+                <img class="img-responsive" src="img/photo1.jpg" alt="Photo1">
+              </div>
+          </div>
+          </div>
+          <div class="sec2">
+          <div class="row">
+               <div class="col-md-6 col-md-push-6 text-center text_sec">
+                  <h3 class="section_h3">Delivery</h3>
+                  <h4 class="section_h4">Here For You</h4>
+                  <a class="transition section_a" href="#">Get in touch</a>
+              </div>
+              <div class="col-md-6 col-md-pull-6">
+                <img class="img-responsive" src="img/photo1.jpg" alt="Photo1">
+              </div>
+          </div>
+          </div>
+           <div class="sec3">
+            <div class="row">
+              <div class="col-md-6 text-center text_sec">
+                  <h3 class="section_h3">Customer Support</h3>
+                  <h4 class="section_h4">Next Level Service</h4>
+                  <a class="transition section_a" href="#">Get in touch</a>
+              </div>
+              <div class="col-md-6">
+                <img class="img-responsive" src="img/photo1.jpg" alt="Photo1">
+              </div>
+            </div>
+          </div>
+          <div class="sec4">
+           <div class="row">
+              <div class="col-md-6">
+                <div class="sec4_1 text-right">
+                <img class="" src="img/3.jpg" alt="Photo1">
+                </div>
+              </div>
+            <div class="col-md-6 text-center">
+              <div class="sec4_2">
+                <h3>Opening Hours</h3>
+                <p>Come Visit</p>
+                <p>Пн - пт: 9 до 6  Сб: з 10 до 6</p>
+                <p>Нд: Закрито</p>
+              </div>
+            </div>
+          </div>
           </div>
       </div>
-    </div><!-- end logo-->
-      <div class="row">
-       <div class="col-md-12">
-          <nav>
-            <div class="col-md-4 col-md-offset-4 nav-menu">
-              <ul>
-                <li><a href="#"><span class="first-menu">Home</span></a></li>
-                <li><a href="#">Shop</a></li>
-                <li><a href="#">Book online</a></li>
-                <li><a href="#">Blog</a></li>
-              </ul>
+      <div class="section2">
+            <div id="itemscarousel" class="carousel slide" data-ride="false">
+              <div class="carousel-inner">
+                <div class="item active">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <a href="#"><img class="img-thumbnail" src="img/10.jpg" alt="photo1"></a>
+                      <div class="carousel-caption">
+                        <h3>Photo1</h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                       <a href="#"><img class="img-thumbnail" src="img/11.jpg" alt="photo1"></a>
+                      <div class="carousel-caption">
+                        <h3>Photo1</h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                       <a href="#"><img class="img-thumbnail" src="img/12.jpg" alt="photo1"></a>
+                      <div class="carousel-caption">
+                        <h3>Photo1</h3>
+                      </div>
+                    </div>
+                  </div>
             </div>
-            <i class="fab fa-instagram"></i>
-          </nav>
+              <div class="item">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <a href="#"><img class="img-thumbnail" src="img/13.jpg" alt="photo1"></a>
+                      <div class="carousel-caption">
+                        <h3>Photo1</h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                       <a href="#"><img class="img-thumbnail" src="img/14.jpg" alt="photo1"></a>
+                      <div class="carousel-caption">
+                        <h3>Photo1</h3>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                       <a href="#"><img class="img-thumbnail" src="img/15.jpg" alt="photo1"></a>
+                      <div class="carousel-caption">
+                        <h3>Photo1</h3>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+          </div>
+             <a href="#itemscarousel" class="carousel-control left" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span>
+             </a>
+              <a href="#itemscarousel" class="carousel-control right" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span>
+              </a>
+          </div>
         </div>
-      </div>
-     <!-- <div class="row">
-        <div class="col-md-4 col-md-offset-4 boots-menu">
-          <ul class="nav justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-             <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-        </div>
-      </div>-->
   </div>
+  
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
     <script src="js/bootstrap.min.js"></script>
+  <!--
+    <script type="text/javascript">
+      //Allows bootstrap carousels to display 3 items per page rather than just one
+$('.carousel.carousel-multi .item').each(function () {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().attr("aria-hidden", "true").appendTo($(this));
+
+  if (next.next().length > 0) {
+    next.next().children(':first-child').clone().attr("aria-hidden", "true").appendTo($(this));
+  }
+  else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+    </script>
+  -->
   </body>
 </html>
